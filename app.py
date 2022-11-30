67333617bc405383a5aa1d6d4765883f9f4b5409
Raghd Al Juma <98473766+Redda1242@users.noreply.github.com>
2022-11-30 16:53:31 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,10 @@ from flask import Flask
 app = Flask(__name__)
  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
  
+ @app.before_first_request
+def create_table():
+    db.create_all()
+  
 app.run(host='localhost', port=5000)
