7539f75dd199468cdf13c271ede94a1ab866e861
Raghd Al Juma <98473766+Redda1242@users.noreply.github.com>
2022-11-30 16:54:31 +0000

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,17 @@ def create_table():
     db.create_all()
   
 app.run(host='localhost', port=5000)
+@app.route('/data/create' , methods = ['GET','POST'])
+def create():
+    if request.method == 'GET':
+        return render_template('createpage.html')
+ 
+    if request.method == 'POST':
+        employee_id = request.form['employee_id']
+        name = request.form['name']
+        age = request.form['age']
+        position = request.form['position']
+        employee = EmployeeModel(employee_id=employee_id, name=name, age=age, position = position)
+        db.session.add(employee)
+        db.session.commit()
+        return redirect('/data')
