5bbfad1e22198d695c676426bf9c0da976680988
Raghd Al Juma <98473766+Redda1242@users.noreply.github.com>
2022-11-30 16:52:31 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
  
 app = Flask(__name__)
+ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
  
 app.run(host='localhost', port=5000)
